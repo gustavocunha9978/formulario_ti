0003bc2c79f31e91d87ad3dc640e96f49f8f0906
elpichula9978 <gustavocunhasoares@hotmail.com>
2023-11-01 08:46:36 -0300

    terceiro commit

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 const nodemailer = require("nodemailer");
 const express = require("express");
-const app = express();
 const bodyParser = require('body-parser');
-require ('dotenv').config();
+const path = require('path');
+const app = express();
+require('dotenv').config();
 
+
+const absolutePath = "C:\\Users\\gustavo.santos\\Documents\\GitHub\\formulario_ti_v2";
+
+app.use(express.static(absolutePath));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post("/sendmail", async (request, response) => {
+app.post("/sendmail", async (req, res) => {
     console.log(request.body);
 
-    
     const formData = request.body;
+    const mail = formData.emailget;
 
     const transport = nodemailer.createTransport({
         host: "smtp.emailarray.com",
@@ -24,17 +29,19 @@ app.post("/sendmail", async (request, response) => {
     try {
         let message = await transport.sendMail({
             from: '"Gustavo Cunha" <ti@sorrisodetoledo.com.br>',
-            to: "telemetria@sorrisodetoledo.com.br, ti@sorrisodetoledo.com.br ", // mudar o email aqui 
+            to: `${mail}, ti@sorrisodetoledo.com.br`, 
             subject: "Formulário",
             text: `
-                Modelo: ${formData.modelo}
+                Item: ${formData.Item}
                 Data de Entrega: ${formData.dataEntrega}
+                Data de Devolução: ${fomatData.dataDevolução}
                 Nome do Responsável pela Entrega: ${formData.responsavel}
                 Nome do Receptor: ${formData.receptor}
                 Departamento/Setor: ${formData.departamento}
                 Observações: ${formData.observacoes}
             `
         });
+
         response.send("Deu boa!");
     } catch (error) {
         console.error("Erro ao enviar o email:", error);
@@ -42,6 +49,11 @@ app.post("/sendmail", async (request, response) => {
     }
 });
 
-app.listen(3001, () => {
-    console.log("Rodando!");
+app.get('/', (req, res) => {
+    res.sendFile(path.join(absolutePath, 'index.html'));
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server on http://10.200.61.180:${PORT}`);
 });
