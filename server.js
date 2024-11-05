946927a3accfbe0b13d444d82fefe9342bfb288f
Gustavo Cunha <104397766+gustavocunha9978@users.noreply.github.com>
2024-11-05 17:23:13 -0300

    config

--- server.js
+++ server.js
@@ -2,13 +2,19 @@ const nodemailer = require("nodemailer");
 const express = require("express");
 const bodyParser = require('body-parser');
 const path = require('path');
+const os = require('os');
 const app = express();
 const cron = require('node-cron');
 const PORT = 3001;
 
+const cors = require('cors');
+app.use(cors());
+
+
 require('dotenv').config();
 
 const ABSOLUTE_PATH = process.env.ABSOLUTE_PATH || path.join(__dirname, 'public');
+const localIP = os.networkInterfaces()['en0']?.find(ip => ip.family === 'IPv4')?.address || 'localhost';
 
 app.use(express.static(ABSOLUTE_PATH));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,8 +28,9 @@ app.post("/sendmail", async (req, res) => {
     const mail = formData.emailget;
 
     const transport = nodemailer.createTransport({
-        host: "smtp.emailarray.com",
-        port: "587",
+        host: "smtp.office365.com",
+        port: 587,
+        secure: false,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
@@ -32,8 +39,8 @@ app.post("/sendmail", async (req, res) => {
 
     try {
         let message = await transport.sendMail({
-            from: '"Gustavo Cunha" <ti@sorrisodetoledo.com.br>',
-            to: `${mail}, ti@sorrisodetoledo.com.br`,
+            from: process.env.EMAIL_USER,
+            to: `${mail}, gustavo.cunha@biopark.com`, //trocar o email se for outra pessoa que for fazer o envio do Formulário
             subject: "Formulário",
             text: `
                 Item: ${formData.Item}
@@ -47,28 +54,27 @@ app.post("/sendmail", async (req, res) => {
         });
 
         if (dataDevolucao && formData.checkbox) {
-            // Enviar e-mail de devolução
             let devolucaoMessage = await transport.sendMail({
-                from: '"Gustavo Cunha" <ti@sorrisodetoledo.com.br>',
-                to: `${mail}, ti@sorrisodetoledo.com.br`,
+                from: '"Gustavo Cunha" <gustavo.cunha@biopark.com.br>',
+                to: `${mail}, gustavo.cunha@biopark.com.br`,
                 subject: "Devolução de Equipamento",
                 text: 'O equipamento que foi emprestado á você precisa ser devolvido até as 17:00h'
             });
             console.log("E-mail de devolução enviado com sucesso:", devolucaoMessage);
         }
 
+        console.log("Deu Boa");
+        res.status(200).json({ status: "success", message: "Deu Boa" });
     } catch (error) {
-        console.error("Erro ao enviar o email:", error);
+        console.error("Deu ruim", error);
+        res.status(500).json({ status: "error", message: "Deu ruim" });
     }
-
-    res.send("<span>OK</span>");
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(ABSOLUTE_PATH, 'index.html'));
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server on http://10.0.8.160:${PORT}`);
+    console.log(`Server on http://${localIP}:${PORT}`);
 });
